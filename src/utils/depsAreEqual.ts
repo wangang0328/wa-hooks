d9cb9a4f7fe6dbd2f6731a6e8247cd9ec8437762
ang <wang3ang@163.com>
2022-12-17 22:21:57 +0800

    feat: add isNil validate

--- src/utils/depsAreEqual.ts
+++ src/utils/depsAreEqual.ts
@@ -1,14 +1,15 @@
-import { isFunction } from './validate'
+import { isFunction, isNil } from './validate'
 // eslint-disable-next-line no-self-compare
 const is = (x: any, y: any) =>
   (x === y && (x !== 0 || 1 / x === 1 / y)) || (x !== x && y !== y)
 
 const objectIs = isFunction(Object.is) ? Object.is : is
 
-const depsAreEqual = (prevDeps: any, nextDeps: any) => {
+const depsAreEqual = (prevDeps: any = null, nextDeps: any = null) => {
+  if (isNil(prevDeps) || isNil(nextDeps)) return false
   if (prevDeps === nextDeps) return true
 
-  if (prevDeps.length !== nextDeps.length) {
+  if (prevDeps?.length !== nextDeps?.length) {
     return false
   }
 
--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -1,12 +1,22 @@
 // 类型校验
-const is = <T>(expectType: string) => (v: unknown): v is T => typeof v === expectType
-export const isObject = (value: unknown): value is Record<any, any> => value !== null && typeof value === 'object'
+const is =
+  <T>(expectType: string) =>
+  (v: unknown): v is T =>
+    typeof v === expectType
+export const isObject = (value: unknown): value is Record<any, any> =>
+  value !== null && typeof value === 'object'
 // eslint-disable-next-line @typescript-eslint/ban-types
 export const isFunction = is<Function>('function')
 export const isString = is<string>('string')
 export const isBoolean = is<boolean>('boolean')
 export const isNumber = is<number>('number')
 export const isUndefined = is<undefined>('undefined')
+export const isNull = is<undefined>('undefined')
+
+export const isNil = (v: unknown): v is undefined | null =>
+  isUndefined(v) || v === null
 
 // 环境校验
-export const isDev = ['development', 'test'].includes(process?.env?.NODE_ENV || '')
+export const isDev = ['development', 'test'].includes(
+  process?.env?.NODE_ENV || '',
+)
